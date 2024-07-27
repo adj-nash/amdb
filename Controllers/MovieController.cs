1ba58c52d48c9997b17484fd13547734e158a3c2
adj-nash <168261681+adj-nash@users.noreply.github.com>
2024-07-27 07:50:00 +0100

    First project commit

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieDatabase.Data;
+using MovieDatabase.Models;
+
+namespace MovieDatabase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly MovieDbContext _dbContext;
+
+        public MovieController(MovieDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int pageIndex = 0, int pageSize = 10)
+        {
+            BaseResponse response = new BaseResponse();
+
+            try
+            {
+                var movieCount = _dbContext.Movie.Count();
+                var movieList = _dbContext.Movie.Include(x => x.Actors).Skip(pageIndex * pageSize).Take(pageSize).ToList();
+
+                response.Status = true;
+                response.Message = "Success";
+                response.Data = new { Movies = movieList, Count = movieCount };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = "Something went wrong.";
+
+                return BadRequest(response);
+
+
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            BaseResponse response = new BaseResponse();
+
+            try
+            {
+                var movie = _dbContext.Movie.Include(x => x.Actors).Where(x => x.Id == id).FirstOrDefault();
+
+                response.Status = true;
+                response.Message = "Success";
+                response.Data = new { Movies = movieList, Count = movieCount };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                response.Status = false;
+                response.Message = "Something went wrong.";
+
+                return BadRequest(response);
+
+
+            }
+        }
+    }
+}
---
+++ Data/MovieDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieDatabase.Entities;
+
+namespace MovieDatabase.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Movie> Movie {  get; set; }
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+    
+}
